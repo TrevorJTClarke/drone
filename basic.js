4666aee9e4eef2d954ea633c752a5d0b8ea33b7d
Trevor Clarke <besnoid@gmail.com>
2015-07-02 18:04:34 -0700

    playing around with navdata and video streams

--- basic.js
+++ basic.js
@@ -1,36 +1,50 @@
 var arDrone = require('ar-drone');
 var http    = require('http');
 
+// var video = arDrone.createClient().getVideoStream();
+//
+// video.on('data', console.log);
+// video.on('error', console.log);
+
 //var pngStream = arDrone.createClient().getPngStream();
 var client = arDrone.createClient();
 client.disableEmergency();
+// client.config();
+client.animateLeds('fire', 5, 2);
+setTimeout(function(){
+  client.animateLeds('standard', 5, 2);
+},3000);
+// client.on('navdata', function (data) {
+//   console.log(data.demo);
+//   console.log("client.config",data.config);
+// });
+//
+// console.log('Connecting png stream ...');
+// var pngStream = client.getPngStream();
+//
+// var lastPng;
+// pngStream
+//   .on('error', console.log)
+//   .on('data', function(pngBuffer) {
+//     lastPng = pngBuffer;
+//   });
+//
+// var server = http.createServer(function(req, res) {
+//   if (!lastPng) {
+//     res.writeHead(503);
+//     res.end('Did not receive any png data yet.');
+//     return;
+//   }
+//
+//   res.writeHead(200, {'Content-Type': 'image/png'});
+//   res.end(lastPng);
+// });
 
-console.log('Connecting png stream ...');
-var pngStream = client.getPngStream();
+// server.listen(8080, function() {
+  // console.log('Serving latest png on port 8080 ...');
+  // client.takeoff();
 
-var lastPng;
-pngStream
-  .on('error', console.log)
-  .on('data', function(pngBuffer) {
-    lastPng = pngBuffer;
-  });
-
-var server = http.createServer(function(req, res) {
-  if (!lastPng) {
-    res.writeHead(503);
-    res.end('Did not receive any png data yet.');
-    return;
-  }
-
-  res.writeHead(200, {'Content-Type': 'image/png'});
-  res.end(lastPng);
-});
-
-server.listen(8080, function() {
-  console.log('Serving latest png on port 8080 ...');
-  client.takeoff();
-
-  client
+  // client
   //   .after(5000, function() {
   //     this.clockwise(0.5);
   //   })
@@ -67,9 +81,9 @@ server.listen(8080, function() {
   //   .after(5000, function() {
   //     this.stop();
   //   })
-    .after(1500, function() {
-      this.stop();
-      this.land();
-    });
+    // .after(1500, function() {
+    //   this.stop();
+    //   this.land();
+    // });
 
-});
+// });
